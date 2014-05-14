1435510dbf8709e5443c4148e3720dd5c38faa38
N. Saphra <nsaphra@gmail.com>
2014-05-14 15:27:05 -0400

    refactoring start with separate smatchgraph module

--- disagree.py
+++ disagree.py
@@ -16,7 +16,6 @@ consecutively in the monolingual annotation file. For bilingual, annotations
 should be in the same order of sentences between the files.
 """
 
-# TODO deal with constant name dupes
 import argparse
 import networkx as nx
 import amr_metadata
@@ -29,137 +28,7 @@ import copy
 import ConfigParser
 from pynlpl.formats.giza import GizaSentenceAlignment
 import codecs
-
-GOLD_COLOR = 'blue'
-TEST_COLOR = 'red'
-DFLT_COLOR = 'black'
-
-def amr_info_to_dict(inst, rel1, rel2):
-  """ Get tables of AMR data indexed by variable number """
-  node_inds = {}
-  inst_t = {}
-  for (ind, (i, v, label)) in enumerate(inst):
-    node_inds[v] = ind
-    inst_t[ind] = label
-
-  rel1_t = {}
-  for (label, v1, const) in rel1:
-    if (node_inds[v1], const) not in rel1_t:
-      rel1_t[(node_inds[v1], const)] = set()
-    rel1_t[(node_inds[v1], const)].add(label)
-
-  rel2_t = {}
-  for (label, v1, v2) in rel2:
-    if (node_inds[v1], node_inds[v2]) not in rel2_t:
-      rel2_t[(node_inds[v1], node_inds[v2])] = set()
-    rel2_t[(node_inds[v1], node_inds[v2])].add(label)
-
-  return (inst_t, rel1_t, rel2_t)
-
-
-def amr_disagree_to_graph(inst, rel1, rel2, gold_inst_t, gold_rel1_t, gold_rel2_t, match, const_map_fn):
-  """ 
-  Input:
-    (inst, rel1, rel2) from test amr.get_triples2()
-    (gold_inst_t, gold_rel1_t, gold_rel2_t) from gold amr_info_to_dict()
-  Returns graph of test AMR / gold AMR union, with hilighted disagreements for
-  different labels on edges and nodes, unmatched nodes and edges.
-  """
-  G = nx.MultiDiGraph()
-  gold_ind = {} # test variable name -> gold variable index
-  tablecopy = lambda x: copy.deepcopy(x)
-  unmatched_gold_inst = tablecopy(gold_inst_t)
-  unmatched_gold_rel1 = tablecopy(gold_rel1_t)
-  unmatched_gold_rel2 = tablecopy(gold_rel2_t)
-
-  for (ind, (i, v, instof)) in enumerate(inst):
-    gold_ind[v] = match[ind]
-
-    node_color = DFLT_COLOR
-    font_color = DFLT_COLOR
-    label = instof
-    if match[ind] < 0:
-      font_color = TEST_COLOR
-      node_color = TEST_COLOR
-    else:
-      if gold_inst_t[match[ind]] != instof:
-        font_color = TEST_COLOR
-        label = "%s (%s)" % (instof, gold_inst_t[match[ind]])
-      if match[ind] in unmatched_gold_inst:
-        del unmatched_gold_inst[match[ind]]
-    G.add_node(v, label=label, color=node_color, font_color=font_color)
-
-  # TODO decision: color all consts appearing in both charts black OR
-  #      have consts hashed according to parent
-  # TODO either expand the number of possible const matches
-  #      or switch to a word-alignment-variant model
-  for (reln, v, const) in rel1:
-    node_color = DFLT_COLOR
-    edge_color = DFLT_COLOR
-    label = const
-    const_match = const_map_fn(const)
-    if (gold_ind[v], const_match) in gold_rel1_t:
-      if const != const_match:
-        label = "%s (%s)" % (const, const_match)
-      if reln not in gold_rel1_t[(gold_ind[v], const_match)]:
-        edge_color = TEST_COLOR
-
-        # relns between existing nodes should be in unmatched rel2
-        gold_ind[const] = const_match
-        unmatched_gold_rel2[(gold_ind[v], const_match)] = unmatched_gold_rel1[(gold_ind[v], const_match)]
-        del unmatched_gold_rel1[(gold_ind[v], const_match)]
-      else:
-        unmatched_gold_rel1[(gold_ind[v], const_match)].remove(reln)
-    else:
-      node_color = TEST_COLOR
-      edge_color = TEST_COLOR
-    # special case: "TOP" specifier not annotated
-    if reln == 'TOP':
-      # find similar TOP edges in gold if they are not labeled with same instance
-      if edge_color == TEST_COLOR:
-        for ((v_, c_), r_) in unmatched_gold_rel1.items():
-          if v_ == gold_ind[v] and 'TOP' in r_:
-            edge_color = DFLT_COLOR
-            unmatched_gold_rel1[(v_, c_)].remove('TOP')
-      G.add_edge(v, v, label=reln, color=edge_color, font_color=edge_color)
-      continue
-    G.add_node(v+' '+const, label=label, color=node_color, font_color=node_color)
-    G.add_edge(v, v+' '+const, label=reln, color=edge_color, font_color=edge_color)
-
-  for (reln, v1, v2) in rel2:
-    edge_color = DFLT_COLOR
-    if (gold_ind[v1], gold_ind[v2]) in gold_rel2_t:
-      if reln not in gold_rel2_t[(gold_ind[v1], gold_ind[v2])]:
-        edge_color = TEST_COLOR
-      else:
-        unmatched_gold_rel2[(gold_ind[v1], gold_ind[v2])].remove(reln)
-    else:
-      edge_color = TEST_COLOR
-    G.add_edge(v1, v2, label=reln, color=edge_color, font_color=edge_color)
-
-  # Add gold standard elements not in test
-  node_hashes = {v:k for (k,v) in gold_ind.items()} # reverse lookup from gold ind
-  for (gold_ind, instof) in unmatched_gold_inst.items():
-    node_hashes[gold_ind] = 'GOLD %s' % gold_ind
-    G.add_node(node_hashes[gold_ind], label=instof, color=GOLD_COLOR, font_color=GOLD_COLOR)
-  for ((gold_ind, const), relns) in unmatched_gold_rel1.items():
-    #TODO check if const node already in
-    for reln in relns:
-      # special case: "TOP" specifier not annotated
-      if reln == 'TOP':
-        G.add_edge(node_hashes[gold_ind], node_hashes[gold_ind], label=reln, color=GOLD_COLOR, font_color=GOLD_COLOR)
-        continue
-
-      const_hash = node_hashes[gold_ind] + ' ' + const
-      if const_hash not in node_hashes:
-        node_hashes[const_hash] = const_hash
-        G.add_node(const_hash, label=const, color=GOLD_COLOR, font_color=GOLD_COLOR)
-      G.add_edge(node_hashes[gold_ind], node_hashes[const_hash], label=reln, color=GOLD_COLOR, font_color=GOLD_COLOR)
-  for ((gold_ind1, gold_ind2), relns) in unmatched_gold_rel2.items():
-    for reln in relns:
-      G.add_edge(node_hashes[gold_ind1], node_hashes[gold_ind2], label=reln, color=GOLD_COLOR, font_color=GOLD_COLOR)
-  return G
-
+import smatch_graph
 
 def hilight_disagreement(test_amrs, gold_amr, aligner=default_aligner):
   """
@@ -173,7 +42,7 @@ def hilight_disagreement(test_amrs, gold_amr, aligner=default_aligner):
   gold_label="b"
   gold_amr.rename_node(gold_label)
   (gold_inst, gold_rel1, gold_rel2) = gold_amr.get_triples2()
-  (gold_inst_t, gold_rel1_t, gold_rel2_t) = amr_info_to_dict(gold_inst, gold_rel1, gold_rel2)
+  (gold_inst_t, gold_rel1_t, gold_rel2_t) = smatch_graph.amr2dict(gold_inst, gold_rel1, gold_rel2)
 
   for a in test_amrs:
     aligner.set_amrs(a, gold_amr)
@@ -184,7 +53,9 @@ def hilight_disagreement(test_amrs, gold_amr, aligner=default_aligner):
       gold_inst, gold_rel1, gold_rel2,
       test_label, gold_label, const_weight_fn=aligner.weight_fn, instance_weight_fn=aligner.weight_fn)
 
-    amr_graphs.append(amr_disagree_to_graph(test_inst, test_rel1, test_rel2, gold_inst_t, gold_rel1_t, gold_rel2_t, best_match, aligner.const_map_fn))
+    amr_graphs.append(smatch_graph.smatch2graph(test_inst, test_rel1, test_rel2, \
+      gold_inst_t, gold_rel1_t, gold_rel2_t, \
+      best_match, aligner.const_map_fn))
   return amr_graphs
 
 
@@ -230,6 +101,7 @@ def monolingual_main(args):
 
 
 def xlang_main(args):
+  """ Disagreement graphs for aligned cross-language language. """
   src_amr_fh = codecs.open(args.src_amr, encoding='utf8')
   tgt_amr_fh = codecs.open(args.tgt_amr, encoding='utf8')
   src2tgt_fh = codecs.open(args.align_src2tgt, encoding='utf8')
