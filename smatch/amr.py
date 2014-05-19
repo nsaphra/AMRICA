62f105230ceb28583c12bd17ed3a6bd4d1cac479
N. Saphra <nsaphra@gmail.com>
2014-05-19 16:41:48 -0400

    cleaned up amr.py a bit

--- smatch/amr.py
+++ smatch/amr.py
@@ -139,57 +139,42 @@ class AMR(object):
             cur_charseq[:]=[]
             parts=temp_attr_value.split()
             if len(parts)<2:
-               print >> sys.stderr, "Error in processing",line[0:i+1]
-               return None
+              print >> sys.stderr, "Error in processing",line[0:i+1]
+              return None
             attr_name=parts[0].strip()
             attr_value=parts[1].strip()
             if len(stack)==0:
-               print >> sys.stderr, "Error in processing",line[:i],attr_name,attr_value
-               return None
+              print >> sys.stderr, "Error in processing",line[:i],attr_name,attr_value
+              return None
             if attr_value not in var_dict:
-               var_attr_dict2[stack[-1]].append((attr_name,attr_value))
-               #print stack[-1],attr_name,attr_value
+              var_attr_dict2[stack[-1]].append((attr_name,attr_value))
             else:
-    #           print stack[-2],attr_name,attr_value
-               var_attr_dict1[stack[-1]].append((attr_name,attr_value))
-            
-            #   if attr_value[0]=="\"" and attr_value[-1]=="\"":
-            #      attr_value=attr_value[1:-1]
-           # attr_dict[attr_name]=attr_value
-       #  elif state==4: #:)....:
-       #     pass
+              var_attr_dict1[stack[-1]].append((attr_name,attr_value))
          state=2
       elif c=="/":
-         if in_quote:
-            continue
-         if state==1:
-            variable_name="".join(cur_charseq)
-            cur_charseq[:]=[]
-            if variable_name in var_dict:
-               print >> sys.stderr, "Duplicate variable ",variable_name, " in parsing AMR"
-               return None
-            stack.append(variable_name)
-            var_list.append(variable_name)
-      #      print variable_name,cur_attr_name
-            if cur_attr_name!="":
-               if not cur_attr_name.endswith("-of"):
-                 # var_attr_dict1[stack[-2]][cur_attr_name]=variable_name
-                  var_attr_dict1[stack[-2]].append((cur_attr_name, variable_name))
-     #             print stack[-2]
-               else:
-                  #print variable_name,cur_attr_name,stack[-2]
-                  var_attr_dict1[variable_name].append((cur_attr_name[:-3],stack[-2]))
-                  #var_attr_dict1[variable_name][cur_attr_name[:-3]]=stack[-2]
-               cur_attr_name=""
-         else:
-            #print state
-            print >> sys.stderr, "Error in parsing AMR", line[0:i+1]
-            return None
-         state=3
+        if in_quote:
+          continue
+        if state==1:
+          variable_name="".join(cur_charseq)
+          cur_charseq[:]=[]
+          if variable_name in var_dict:
+             print >> sys.stderr, "Duplicate variable ",variable_name, " in parsing AMR"
+             return None
+          stack.append(variable_name)
+          var_list.append(variable_name)
+          if cur_attr_name!="":
+            if not cur_attr_name.endswith("-of"):
+              var_attr_dict1[stack[-2]].append((cur_attr_name, variable_name))
+            else:
+              var_attr_dict1[variable_name].append((cur_attr_name[:-3],stack[-2]))
+            cur_attr_name=""
+        else:
+          print >> sys.stderr, "Error in parsing AMR", line[0:i+1]
+          return None
+        state=3
       elif c==")": 
          if in_quote:
             continue
-         #pop
          if len(stack)==0:
             print >> sys.stderr, "Unmatched parathesis at position", i, "in processing", line[0:i+1]
             return None
@@ -206,7 +191,6 @@ class AMR(object):
                var_attr_dict1[variable_name].append((cur_attr_name[:-3],stack[-2])) 
             elif attr_value not in var_dict:
                var_attr_dict2[stack[-1]].append((attr_name,attr_value))
-               #print stack[-1],attr_name,attr_value
             else:
                var_attr_dict1[stack[-1]].append((attr_name,attr_value))
          elif state==3:
@@ -214,11 +198,7 @@ class AMR(object):
             cur_charseq[:]=[]
             cur_var_name=stack[-1]
             var_dict[cur_var_name]=var_value
-         stack.pop() 
-       #  if cur_attr_name!="":
-       #     var_attr_dict[cur_attr_name]= 
-       #  var_name.append(cur_var_name)
-       #  attr_list.append(attr_dict)
+         stack.pop()
          cur_attr_name=""
          state=4 
       else:
@@ -239,12 +219,6 @@ class AMR(object):
       if v in var_attr_dict1:
          for v1 in var_attr_dict1[v]:
            link_dict[v1[1]]=v1[0]
-         #for k1,v1 in var_attr_dict1[v].items():
-          # if v in var_list:
-       #    link_dict[v1]=k1
-         #  else:
-         #     const_dict[v2]=k
-    #     print v, var_attr_dict2[v]
       if v in var_attr_dict2:
          for v2 in var_attr_dict2[v]:
            if v2[1][0]=="\"" and v2[1][-1]=="\"":
@@ -253,17 +227,8 @@ class AMR(object):
               link_dict[v2[1]]=v2[0]
            else:
               const_dict[v2[0]]=v2[1]
-       #  for k2,v2 in var_attr_dict2[v].items():
-       #    if v2[0]=="\"" and v2[1]=="\"":
-       #       const_dict[k2]=v2[1:-1]
-        #   elif v2 in var_dict:
-      #        print "test",v,v2,k2
-        #      link_dict[v2]=k2
-        #   else:
-        #      const_dict[k2]=v2
       link_list.append(link_dict)
       const_attr_list.append(const_dict)
       const_attr_list[0]["TOP"]=var_value_list[0] # add TOP relation
     result_amr=AMR(var_list,var_value_list,link_list,const_attr_list)
-    #print result_amr.links
     return result_amr
